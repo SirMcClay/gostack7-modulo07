5edfce3996a94bb58e856c37d3c9d1eb90e0627e
sirmcclay <mcclay.design@gmail.com>
2019-12-19 05:25:56 -0300

    Cart styles finished

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -33,13 +33,15 @@ export default function Cart() {
               <span>R$129,90</span>
             </td>
             <td>
-              <button type="button">
-                <MdRemoveCircleOutline size={20} color="#7159c1" />
-              </button>
-              <input type="number" readOnly value={1} />
-              <button type="button">
-                <MdAddCircleOutline size={20} color="#7159c1" />
-              </button>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
             </td>
             <td>
               <strong>R$258,80</strong>
