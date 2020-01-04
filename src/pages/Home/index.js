20baf5bc4bb7673cbc9145065cc9bf7ee02a8263
sirmcclay <mcclay.design@gmail.com>
2020-01-04 04:48:14 -0300

    Home products from API Json-server

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 
 import { ProductList } from './styles';
@@ -12,7 +13,12 @@ export default class Home extends Component {
   async componentDidMount() {
     const response = await api.get('products');
 
-    this.setState({ products: response.data });
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
   }
 
   render() {
@@ -21,13 +27,10 @@ export default class Home extends Component {
     return (
       <ProductList>
         {products.map(product => (
-          <li>
-            <img
-              src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/28/COL-3586-128/COL-3586-128_detalhe2.jpg?ims=326x"
-              alt="Tênis"
-            />
-            <strong>Tênis muito legal</strong>
-            <span>R$129,90</span>
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
             <button type="button">
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" /> 3
